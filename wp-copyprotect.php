24017bae770b3208d9e4873413b3bf3cb71859d5
cchetanonline <cchetanonline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-26 17:31:37 +0000

    Release for version 2.0.0
    1. Now we have option to disable right click without showing any message to user. [new] 
    2. Setting panel UI changed. [improved]
    3. Java script for copy protection is optimized for quick loading. [improved]
    4. Code is enhanced with proper commenting and re-factoring variables. [improved]
    5. All URLs mentioned in plug-in are changed as <a href="http://chetangole.com/blog/?p=1938">my blog URL is changed</a>.[new]
    6. Compatibility tested up to Wordpress version 3.0.1 [regular]
    
    git-svn-id: https://plugins.svn.wordpress.org/wp-copyprotect/trunk@304622 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-copyprotect.php
+++ wp-copyprotect.php
@@ -1,44 +1,38 @@
 <?php
 /*
-Plugin Name: WP-CopyProtect [Protect your posts]
-Plugin URI: http://www.thechetan.com/wp-copyprotect/
+Plugin Name: WP-CopyProtect [Protect your blog posts]
+Plugin URI: http://chetangole.com/blog/wp-copyprotect/
 Description: This plug-in will protect your blog content [posts] from being copied. A simple plug-in developed to stop the Copy cats.
-Version: 1.9.1
+Version: 2.0.0
 Author: Chetan Gole
-Author URI: http://www.thechetan.com/
+Author URI: http://chetangole.com/
 */
 
 /*
-Copyright 2009-10  Chetan Gole, IN  (http://www.thechetan.com/)
+Copyright (C) 2010  Chetan Gole - chetangole.com
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details: http://www.gnu.org/licenses/gpl.txt
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-
-
-// No right click - Problem for copy cats NO RIGHT CLICK
+// No right click (with message) - Problem for copy cats NO RIGHT CLICK
 function CopyProtect_no_right_click($CopyProtect_click_message)
 {
 ?>
 <script type="text/javascript">
 <!--
-
-/***************************************************************************************************************
-*Copyprotection for this site is provided by WP-CopyProtect v1.9.1 visit www.TheChetan.com/wp-copyprotect/ for more details
-*RightClick Disabled, Please DO NOT COPY.
-***************************************************************************************************************/
-
 var message="<?php echo $CopyProtect_click_message; ?>";
-
-///////////////////////////////////
 function clickIE4(){
 if (event.button==2){
 alert(message);
@@ -62,24 +56,52 @@ document.onmousedown=clickNS4;
 else if (document.all&&!document.getElementById){
 document.onmousedown=clickIE4;
 }
-
 document.oncontextmenu=new Function("alert(message);return false")
-
 // --> 
 </script>
 
 <?php
 }
+// No right click (without message) - Problem for copy cats NO RIGHT CLICK
+function CopyProtect_no_right_click_without_message()
+{
+?>
+<script type="text/javascript">
+<!--
+function clickIE4(){
+if (event.button==2){
+return false;
+}
+}
 
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("return false")
+// --> 
+</script>
+
+<?php
+}
 // No selection header - Now your content is protected from copy and paste guys
 function CopyProtect_no_select()
 {
 ?>
 <script type="text/javascript">
-/***************************************************************************************************************
-*Copyprotection for this site is provided by WP-CopyProtect v1.9.1 visit www.TheChetan.com/wp-copyprotect/ for more details
-*Selection Disabled, Please DO NOT COPY.
-**************************************************************************************************************/
+
 function disableSelection(target){
 if (typeof target.onselectstart!="undefined") //For IE 
 	target.onselectstart=function(){return false}
@@ -94,9 +116,7 @@ target.style.cursor = "default"
 
 <?php
 }
-
 // No selection footer 
-// Yes, You can remove this link from footer, but why ?, This plugin is protecting your content, so just donate me a backlink.
 // If you are removing this link please consider adding me in your blogroll or write a post about this plugin.
 function CopyProtect_no_select_footer()
 {
@@ -104,51 +124,51 @@ function CopyProtect_no_select_footer()
 <script type="text/javascript">
 disableSelection(document.body)
 </script>
-<small>Copy Protected by <a href="http://www.thechetan.com/" target="_blank">Chetan</a>'s <a href="http://www.thechetan.com/wp-copyprotect/" target="_blank">WP-CopyProtect</a>.</small>
+<small>Copy Protected by <a href="http://chetangole.com/" target="_blank">Chetan</a>'s <a href="http://chetangole.com/blog/wp-copyprotect/" target="_blank">WP-Copyprotect</a>.</small>
 <?php
 }
-
 
 // Tuning your WP-CopyProtect
 function CopyProtect_options_page()
 {
+?>
+<div class="wrap">
+	<h1>WP-CopyProtect <font size="2">ver 2.0.0</font></h1> 
+	| <a href="http://chetangole.com/blog/wp-copyprotect/" target="_blank" title="Visit homepage of wordpress plugin WP-CopyProtect">Visit Plugin page</a> | <a href="http://chetangole.com/blog/wp-copyprotect/#donate" target="_blank" title="Donate some amount to WP-CopyProtect plugin developer to help him to develope more such plugins">Donate</a> | <a href="http://chetangole.com/blog/wp-copyprotect/#donors" target="_blank" title="Few power donors,special thanks to them">Power Donors</a> | 
+	<h3>- settings page -</h3>
+<?php
 	if($_POST['CopyProtect_save']){
 		update_option('CopyProtect_nrc',$_POST['CopyProtect_nrc']);
 		update_option('CopyProtect_nts',$_POST['CopyProtect_nts']);
 		update_option('CopyProtect_nrc_text',$_POST['CopyProtect_nrc_text']);
 
-		echo '<div class="updated"><p>Commands accepted</p></div>';
+		echo '<div class="updated"><p>Settings saved</p></div>';
 	}
 	$wp_CopyProtect_nrc = get_option('CopyProtect_nrc');
 	$wp_CopyProtect_nts = get_option('CopyProtect_nts');
 	?>
-	<div class="wrap">
-	<h1>WP-CopyProtect</h1> ver. 1.9.1
-	| <a href="http://www.thechetan.com/wp-copyprotect/" target="_blank" title="Visit homepage of wordpress plugin WP-CopyProtect">Visit Plugin page</a> | <a href="http://www.thechetan.com/wp-copyprotect/#donate" target="_blank" title="Donate some amount to WP-CopyProtect plugin developer to help him to develope more such plugins">Donate</a> | <a href="http://www.thechetan.com/wp-copyprotect/#donors" target="_blank" title="Few power donors,special thanks to them">Power Donors</a> | 
-	<h2>WP-CopyProtect Options</h2>
 	<form method="post" id="CopyProtect_options">
 		<fieldset class="options">
-		<legend>Now, its the time to bang the copy cats.</legend>
-		<legend>Select the proper options as per your needs</legend>
-		<table class="form-table">
-			
+		<table class="form-table">		
 			<tr valign="top"> 
 				<th width="33%" scope="row">Disable right mouse click:</th> 
 				<td>
-				<input type="checkbox" id="CopyProtect_nrc" name="CopyProtect_nrc" value="CopyProtect_nrc" <?php if($wp_CopyProtect_nrc == true) { echo('checked="checked"'); } ?> />
-				check to activate <br />
+				<input type="radio" name="CopyProtect_nrc" value="0" <?php if($wp_CopyProtect_nrc == 0) echo('checked'); ?> />
+				Do not disable right click.<br />
+				<input type="radio" name="CopyProtect_nrc" value="1" <?php if($wp_CopyProtect_nrc == 1) echo('checked'); ?> />
+				Disable right click, no message.<br />
+				<input type="radio" name="CopyProtect_nrc" value="2" <?php if($wp_CopyProtect_nrc == 2) echo('checked'); ?> />
+				Disable right click and show message :
 				<input name="CopyProtect_nrc_text" type="text" id="CopyProtect_nrc_text" value="<?php echo get_option('CopyProtect_nrc_text') ;?>" size="30"/>
-				This warning will be given to right clickers.
 				</td> 
-			</tr>
+			</tr> 
 			<tr valign="top"> 
 				<th width="33%" scope="row">Disable text selection:</th> 
 				<td>
 				<input type="checkbox" id="CopyProtect_nts" name="CopyProtect_nts" value="CopyProtect_nts" <?php if($wp_CopyProtect_nts == true) { echo('checked="checked"'); } ?> />
-				check to activate. <a href="http://www.thechetan.com/wp-copyprotect/#kp" target="_blank">Not working ?</a>
+				Activate. <a href="http://www.thechetan.com/wp-copyprotect/#kp" target="_blank">Not working ?</a>
 				</td> 
-			</tr>
-			
+			</tr>			
 		<tr>
         <th width="33%" scope="row">Save settings :</th> 
         <td>
@@ -156,28 +176,18 @@ function CopyProtect_options_page()
         </td>
         </tr>
 		<tr>
-        <th width="33%" scope="row">Please note :</th> 
-        <td>
-		
-This is just a basic copy protect plug-in, if someone want to copy your content he/she can go to source of the blog and can easily copy the stuff from there. 
-Most copy cats use your blogs <a href="/feed/" target="_blank">RSS feeds</a> to steal the content. Always select "Summary" at "For each article in a feed, show" in Wordpress admin panel "<a href="options-reading.php">Reading Settings</a>" so that even if someone try to copy your content from feeds he/she can not copy the whole post.
-		
-		</td>
-        </tr>
-		
-		<tr>
         <th scope="row" style="text-align:right; vertical-align:top;">
         <td>
 		<h3>Whats next ?</h3>
-		<p>Why don't you <a href="/wp-admin/post-new.php">write a post</a> about <a href="http://www.thechetan.com/wp-copyprotect/" target="_blank">WP-CopyProtect</a> ?</p>
+		<p>Why don't you <a href="/wp-admin/post-new.php">write a post</a> about <a href="http://chetangole.com/blog/wp-copyprotect/" target="_blank">WP-CopyProtect</a> ?</p>
 		<h3>Problems, Questions, Suggestions ?</h3>
-		<p>Catch me on <a href="http://www.thechetan.com/wp-copyprotect/" target="_blank">WP-CopyProtect Homepage</a></p>
+		<p>Send me mail via: <a href="http://chetangole.com/blog/wp-copyprotect/" target="_blank">WP-CopyProtect Homepage</a></p>
         </td>
         </tr>
 		</table>
 		<h3>Thank you</h3>
-		Plug in developed by <a href="http://www.thechetan.com/" target="_blank">Chetan Gole</a>. <br />
-		<small>Follow me on Twitter <a href="http://twitter.com/Chetan_Gole" target="_blank">@Chetan_Gole</a></small>
+		Plug in developed by <a href="http://chetangole.com/blog/" target="_blank">Chetan Gole</a>. <br />
+		<small>Follow me on Twitter <a href="http://twitter.com/_techie" target="_blank">@_techie</a></small>
 		</fieldset>
 	</form>
 	</table>
@@ -185,17 +195,18 @@ Most copy cats use your blogs <a href="/feed/" target="_blank">RSS feeds</a> to 
 	<?php
 }
 
-//We are calling you, function
+//call to function
 function CopyProtect()
 {
-
 	$wp_CopyProtect_nrc = get_option('CopyProtect_nrc');
+	$wp_CopyProtect_nrc_wo_msg = get_option('CopyProtect_nrc_wo_msg');
 	$wp_CopyProtect_nts = get_option('CopyProtect_nts');
 	$wp_CopyProtect_nrc_text = get_option('CopyProtect_nrc_text');
 	$pos = strpos(strtolower(getenv("REQUEST_URI")), '?preview=true');
 	
 	if ($pos === false) {
-		if($wp_CopyProtect_nrc == true) { CopyProtect_no_right_click($wp_CopyProtect_nrc_text); }
+		if($wp_CopyProtect_nrc == 1) { CopyProtect_no_right_click_without_message(); }
+		if($wp_CopyProtect_nrc == 2) { CopyProtect_no_right_click($wp_CopyProtect_nrc_text); }
 		if($wp_CopyProtect_nts == true) { CopyProtect_no_select(); }
 	}
 }
@@ -214,7 +225,7 @@ function CopyProtect_adminmenu()
 	}
 }
 
-//  Commanding the Wordpress
+//Commanding the Wordpress
 add_action('wp_head','CopyProtect');
 add_action('wp_footer','CopyProtect_footer');
 add_action('admin_menu','CopyProtect_adminmenu',1);
